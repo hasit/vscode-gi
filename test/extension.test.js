d027a37ef07c110bf56d155760afc755ffc11e98
Hasit Mistry <hasitnm@gmail.com>
2017-04-12 16:56:03 -0700

    Update travis file

--- test/extension.test.js
+++ test/extension.test.js
@@ -15,7 +15,6 @@ var myExtension = require('../extension');
 
 // Defines a Mocha test suite to group tests of similar kind together
 suite("Extension Tests", function() {
-
     // Defines a Mocha unit test
     test("Something 1", function() {
         assert.equal(-1, [1, 2, 3].indexOf(5));
